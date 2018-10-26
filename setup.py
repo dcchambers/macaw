fe6dc16bea6038a33d22173cf1a468d1fe4f64dc
Dakota Chambers <dakotachambers@gmail.com>
2018-10-25 21:28:37 -0500

    Add PyPi Package configuration and License.
    Added setup.py and __init__.py used in PyPi packaging.
    Added a License (MIT).
    Moved some files around to the correct location.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="macawspeak",
+    version="0.0.1",
+    author="Dakota Chambers",
+    author_email="dakotachambers@gmail.com",
+    description="Better password generation inspired by our colorful, feathered friends.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dcchambers/macaw",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
