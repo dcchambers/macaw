901af84a8094e0308e58f08504c28d9d162c5d96
Dakota Chambers <dakotachambers@gmail.com>
2018-10-25 22:09:35 -0500

    Add entry_point to setup.py and define main function.

--- macaw/macaw.py
+++ macaw/macaw.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 # Macaw
 #
 # Testing file open and string concatenation.
-password = ''
 
-f = open('dictionary.txt')
-for word in f.read().split():
-    password = password + word
-
-print(password)
+def main():
+    password = ''
+    f = open('dictionary.txt')
+    for word in f.read().split():
+        password = password + word
+    print(password)
--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="macawspeak",
+#    packages=["macaw"],
     version="0.0.1",
     author="Dakota Chambers",
     author_email="dakotachambers@gmail.com",
@@ -13,9 +14,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dcchambers/macaw",
     packages=setuptools.find_packages(),
+    keywords = [
+		"password",
+        "macaw",
+	],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points = {
+		"console_scripts": [
+			"macaw=macaw.macaw:main"
+		]
+	}
 )
