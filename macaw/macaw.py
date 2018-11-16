1d5b4799ee192298b19a29b47185fccc9f9eed1a
Dakota Chambers <dakotachambers@gmail.com>
2018-11-15 19:05:25 -0600

    Add random library and logic to pull random words from dictionary.

--- macaw/macaw.py
+++ macaw/macaw.py
@@ -4,15 +4,20 @@
 #
 # Testing file open and string concatenation.
 
+import random
+
 def main():
     password = ''
+
     # This dictionary of words is for testing only and should *not* be considered secure.
     # Courtesy of https://gist.github.com/deekayen/4148741
     f = open('dictionary.txt')
     wordList = f.read().split()
-    for i in range(0, 4):
-        word = wordList[i]
-        password = password + word
+
+    for i in range(0, 5):
+        word = wordList[random.randint(0,999)] # grab a random word from the dictionary file.
+        password = password + word #concat that word to the end of the password.
+        
     print(password)
     #print("Macaw!") #DEBUG
 
