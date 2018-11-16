f9a0ff310211a1bc07e5a6d970b1ff0f5b1bebaf
Dakota Chambers <dakotachambers@gmail.com>
2018-11-15 18:59:46 -0600

    Add new dictionary and start password generation logic.

--- macaw/macaw.py
+++ macaw/macaw.py
@@ -5,9 +5,15 @@
 # Testing file open and string concatenation.
 
 def main():
-    #password = ''
-    #f = open('dictionary.txt')
-    #for word in f.read().split():
-    #    password = password + word
-    #print(password)
-    print("Macaw!")
+    password = ''
+    # This dictionary of words is for testing only and should *not* be considered secure.
+    # Courtesy of https://gist.github.com/deekayen/4148741
+    f = open('dictionary.txt')
+    wordList = f.read().split()
+    for i in range(0, 4):
+        word = wordList[i]
+        password = password + word
+    print(password)
+    #print("Macaw!") #DEBUG
+
+main()
