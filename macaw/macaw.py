504de2b4dfeca2afa712dc8a3b9a5f2ddfbc9549
Dakota Chambers <dakotachambers@gmail.com>
2018-11-15 19:17:31 -0600

    Remove password generation logic from main function.

--- macaw/macaw.py
+++ macaw/macaw.py
@@ -7,18 +7,26 @@
 import random
 
 def main():
-    password = ''
-
+    
     # This dictionary of words is for testing only and should *not* be considered secure.
     # Courtesy of https://gist.github.com/deekayen/4148741
     f = open('dictionary.txt')
     wordList = f.read().split()
 
+    password = generatePassword(wordList)
+    speakPassword(password)
+
+    #print("Macaw!") #DEBUG
+
+
+def speakPassword(str):
+    print(str)
+
+def generatePassword(wordList):
+    tempPass = ''
     for i in range(0, 5):
         word = wordList[random.randint(0,999)] # grab a random word from the dictionary file.
-        password = password + word #concat that word to the end of the password.
-        
-    print(password)
-    #print("Macaw!") #DEBUG
+        tempPass = tempPass + word #concat that word to the end of the password.
+    return tempPass
 
 main()
